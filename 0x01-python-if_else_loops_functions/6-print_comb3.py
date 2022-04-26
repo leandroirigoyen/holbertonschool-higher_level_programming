bec193aef974ec02a873d8937b5da4818df3adce
sambi0197 <leandro.irigoyen97@gmail.com>
2022-04-26 16:02:14 -0700

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 for number in range(0, 9):
-    for number2 in range(0, 9):
-        if number != number2:
-            print(number, number2, end=', ')
+    for number2 in range(0, 10):
+        if number2 > number:
+            if number==8 and number2==9:
+                print(number, number2, sep='')
+            else:
+                print(number, number2, end=', ', sep='')
