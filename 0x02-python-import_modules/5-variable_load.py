581e7896248cbf655870ea43eda83d96b0958571
sambi0197 <leandro.irigoyen97@gmail.com>
2022-04-28 16:02:22 -0700

    python

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-if _name_ == "_main_":
-    print(f"{a}")
+if __name__ == "__main__":
+    from variable_load_5 import a
+
+    if _name_ == "_main_":
+        print(f"{a}")
