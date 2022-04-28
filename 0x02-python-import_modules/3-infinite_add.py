c33c89a733c81582aad7c2666a30878f22e7a655
sambi0197 <leandro.irigoyen97@gmail.com>
2022-04-28 08:39:54 -0700

    python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,10 +4,7 @@ if __name__ == "__main__":
     h = 0
     largo = len(sys.argv)
 
-    if len(sys.argv) == 1:
-        print(0)
-    else:
-        for c in range(1, largo):
-            h = h + int(sys.argv[c])
+    for c in range(1, largo):
+        h = h + int(sys.argv[c])
 
     print("{}".format(h))
