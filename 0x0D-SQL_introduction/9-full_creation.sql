bcb1aa0d742bec59c894ed033c99c1290b1f2f4c
sambi0197 <leandro.irigoyen97@gmail.com>
2022-07-06 11:14:34 -0700

    mysql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Create a secon table in database hbn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
-INSERT INTO second_table (id,name,score) VALUES (1,'John',10);
-INSERT INTO second_table (id,name,score) VALUES (2,'Alex',3);
-INSERT INTO second_table (id,name,score) VALUES (1,'Bob',14);
-INSERT INTO second_table (id,name,score) VALUES (1,'George',8);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (1,'John',10);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (2,'Alex',3);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (1,'Bob',14);
+INSERT INTO `second_table` (`id`,`name`,`score`) VALUES (1,'George',8);
