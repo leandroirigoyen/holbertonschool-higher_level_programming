c9ca255823c9e62f8c3ffa5b6c4c913b18ee0e7d
leandroirigoyen <leandro.irigoyen97@gmail.com>
2022-08-01 16:19:38 -0700

    javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const x = 'C is fun\n';
-const y = 'Python is cool\n';
-const z = 'JavaScript is amazing\n';
-console.log(x,y,z);
+let str = `C is fun
+Python is cool
+JavaScript is amazing`;
+console.log(str);
