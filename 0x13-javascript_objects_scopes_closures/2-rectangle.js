a70de77b41b9a5df1663c4644d3caa7f5cbc8527
leandroirigoyen <leandro.irigoyen97@gmail.com>
2022-08-03 15:32:41 -0700

    objectscopes

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) && (h = parseInt(h))) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
