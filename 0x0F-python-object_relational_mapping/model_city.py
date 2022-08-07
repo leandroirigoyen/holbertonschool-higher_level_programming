8f252e0cd209de2cd17371baabfa70157680e0bd
sambi0197 <leandro.irigoyen97@gmail.com>
2022-08-07 15:37:31 -0700

    mapping

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,9 +9,10 @@ Base = declarative_base()
 
 
 class City(Base):
-        '''Represents a city for a MySQL database.
-        '''
-        __tablename__ = "cities"
-        id = Column(Integer, primary_key=True)
-        name = Column(String(128), nullable=False)
-        state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    '''
+    Represents a city for a MySQL database.
+    '''
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
