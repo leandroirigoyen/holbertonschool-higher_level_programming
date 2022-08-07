47e18470d0b2a27d56b3129906d85f76a021718f
sambi0197 <leandro.irigoyen97@gmail.com>
2022-08-07 13:16:18 -0700

    mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Write a script that lists all states from the database hbtn_0e_0_usa
+'''
+Write a script that lists all states from the database hbtn_0e_0_usa
+'''
 import sys
 import MySQLdb
 
