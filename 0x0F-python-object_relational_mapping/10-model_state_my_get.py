d2660cddf9a086e147f5a8be636305b03cca74e1
sambi0197 <leandro.irigoyen97@gmail.com>
2022-08-07 15:02:45 -0700

    mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-Write a script that prints the State object with the 
+Write a script that prints the State object with the
 name passed as argument from the database hbtn_0e_6_usa
 '''
 import sys
